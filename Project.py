9cd674610e5e4c486962548ce418c261b98008a2
niemaattarian <G00346901@gmit.ie>
2019-03-09 20:33:11 +0100

    Third Commit - Implementing Thompson's Construction

--- Project.py
+++ Project.py
@@ -52,3 +52,70 @@ def shunt(infix):
   return pofix
 
 print(shunt("(a.b)|(c*.d)"))
+
+# Thompsons construction Algorithm
+
+class state:
+  label = None
+  edge1 = None
+  edge2 = None
+
+class nfa:
+  initial = None
+  accept = None
+
+  def __init__(self, initial, accept):
+    self.initial = initial
+    self.accept = accept
+
+def compile(pofix):
+  nfastack = []
+
+  for c in pofix:
+    if c == '*':
+      nfa1.nfastack.pop()
+      initial = state()
+      accept = state()
+      initial.edge1 = nfa1.initial
+      initial.edge2 = accept
+      nfa1.accept.edge1 = nfa.initial
+      nfa1.accept.edge2 = accept
+      nfastack.append(nfa(initial, accept))
+
+      newnfa = nfa(initial, accept)
+      nfastack.append(newnfa)
+
+    elif c == '.':
+      nfa2 = nfastack.pop()
+      nfa1 = nfastack.pop()
+      nfa1.accept.edge1 = nfa2.initial
+      newnfa = nfa(nfa1.initial, nfa2.accept)
+      nfastack.append(newnfa)
+
+    elif c == '|':
+      nfa2 = nfastack.pop()
+      nfa1 = nfastack.pop()
+
+      initial = state()
+      initial.edge1 = nfa1.initial
+      initial.edge2 = nfa2.initial
+      accept = state()
+
+      nfa1.accept.edge1 = accept
+      nfa2.accept.edge1 = accept
+
+      newnfa = nfa(initial, accept)
+      nfastack.append(newnfa)
+
+    else:
+      accept = state()
+      initial = state()
+      initial.label = c
+      initial.edgel = accept
+      newnfa = nfa(initial, accept)
+      nfastack.append(newnfa)
+
+  return nfastack.pop()
+
+print(compile("ab.cd.|"))
+print(compile("aa.*"))
