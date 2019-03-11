e7db78674f666952626f3b4d51bedbefb3b8dcda
niemaattarian <G00346901@gmit.ie>
2019-03-11 18:53:49 +0100

    Fourth Commit - Understanding Thompson's Construction (via comments) and adding '+' and '?' to the algorithm

--- Project.py
+++ Project.py
@@ -13,7 +13,7 @@ def shunt(infix):
   # ? = 0 or 1
   # | = or
   # . = concatenate
-  specials = {'*': 60, '.': 40, '|': 20}
+  specials = {'*': 60, '+': 60, '?': 60, '.': 40, '|': 20}
 
   # Initializing empty pofix and stack strings
   pofix = ""
@@ -41,7 +41,10 @@ def shunt(infix):
         stack = stack[:-1]
       stack = stack + c
     else:
-      pofix = pofix + c # Appending the character read in from the infix regular expression into the pofix regualr expression
+      # Appending the character read in
+      # from the infix regular expression into
+      # the pofix regualr expression
+      pofix = pofix + c
 
   while stack:
     pofix = pofix + stack[-1]
@@ -51,71 +54,143 @@ def shunt(infix):
   # returns pofix argument
   return pofix
 
+# Examples
 print(shunt("(a.b)|(c*.d)"))
+print(shunt("(a|b.c).c*"))
 
 # Thompsons construction Algorithm
 
+# State class - Represents a state with two arrows,
+# labelled by label.
 class state:
+  # Note that each variable have been
+  # set to none to assign no value to each.
   label = None
   edge1 = None
   edge2 = None
 
+# Nfa class
 class nfa:
-  initial = None
-  accept = None
+  initial = None # initial nfa state
+  accept = None # single accept nfa state
 
-  def __init__(self, initial, accept):
+  # Constructor inside nfa
+  def __init__(self, initial, accept): # self represents the current instance of the class
     self.initial = initial
     self.accept = accept
 
+# Compile function - this takes the postfix
+# regular expression as the argument
 def compile(pofix):
-  nfastack = []
+  nfastack = [] # empty list
 
+  # looping through the postfix expression
+  # one character at a time
   for c in pofix:
-    if c == '*':
-      nfa1.nfastack.pop()
-      initial = state()
-      accept = state()
-      initial.edge1 = nfa1.initial
-      initial.edge2 = accept
-      nfa1.accept.edge1 = nfa.initial
-      nfa1.accept.edge2 = accept
-      nfastack.append(nfa(initial, accept))
+    # If c is the 'concatenate' operator
+    if c == '.':
+      nfa2 = nfastack.pop() # goes to stack, modifies array and returns it back
+      nfa1 = nfastack.pop() # NOTE: stacks a re L.I.F.O.
 
-      newnfa = nfa(initial, accept)
-      nfastack.append(newnfa)
+      nfa1.accept.edge1 = nfa2.initial # Merging the accept state of nfa1 to the initial state of nfa2
 
-    elif c == '.':
-      nfa2 = nfastack.pop()
-      nfa1 = nfastack.pop()
-      nfa1.accept.edge1 = nfa2.initial
-      newnfa = nfa(nfa1.initial, nfa2.accept)
-      nfastack.append(newnfa)
+      newnfa = nfa(nfa1.initial, nfa2.accept) # This is now the new nfa
+      nfastack.append(newnfa) # Appending the new nfa to the stack
 
+    # If c is the 'or' operator
     elif c == '|':
-      nfa2 = nfastack.pop()
+      nfa2 = nfastack.pop() # Popping NFA's off the stack
       nfa1 = nfastack.pop()
 
-      initial = state()
+      initial = state() # creates the initial state
       initial.edge1 = nfa1.initial
       initial.edge2 = nfa2.initial
-      accept = state()
+      accept = state() # creates new accept state connecting the accept states
 
-      nfa1.accept.edge1 = accept
+      nfa1.accept.edge1 = accept #
       nfa2.accept.edge1 = accept
 
       newnfa = nfa(initial, accept)
       nfastack.append(newnfa)
 
-    else:
-      accept = state()
+    # If c is the 'kleene star' operator
+    elif c == '*':
+      # Pop single nfa from the stack
+      nfa1 = nfastack.pop()
+
+      # Creating new initial and accept state
       initial = state()
-      initial.label = c
-      initial.edgel = accept
+      accept = state()
+
+      # Join the new initial state to nfa's
+      # initial state and new accept state
+      initial.edge1 = nfa1.initial
+      initial.edge2 = accept
+
+      # Join old accept state to the new accept state and nfa's initial state
+      nfa1.accept.edge1 = nfa1.initial
+      nfa1.accept.edge2 = accept
+
+      newnfa = nfa(initial, accept)
+      nfastack.append(newnfa) # Appending new nfa to the stack
+
+    # if c is the '?' operator
+    elif c == '?':
+      # Pop single nfa from the stack
+      nfa1 = nfastack.pop()
+
+      # Creating new initial and accept state
+      initial = state()
+      accept = state()
+
+      # Join the new initial state to nfa's
+      # initial state and new accept state
+      initial.edge1 = nfa1.initial
+      initial.edge2 = accept
+
+      # Join old accept state to the new accept state and nfa's initial state
+      nfa1.accept.edge1 = nfa1.initial
+      nfa1.accept.edge2 = accept
+
+      newnfa = nfa(initial, accept)
+      nfastack.append(newnfa) # Appending new nfa to the stack
+
+    # If c is the 'plus' operator
+    elif c == '+':
+      # Pop single nfa from the stack
+      nfa1 = nfastack.pop()
+
+      # Creating new initial and accept state
+      initial = state()
+      accept = state()
+
+      # Join the new initial state to nfa's
+      # initial state and new accept state
+      initial.edge1 = nfa1.initial
+      initial.edge2 = accept
+
+      # Join old accept state to the new accept state and nfa's initial state
+      nfa1.accept.edge1 = nfa1.initial
+      nfa1.accept.edge2 = accept
+
+      newnfa = nfa(initial, accept)
+      nfastack.append(newnfa)  # Appending new nfa to the stack
+
+    else:
+      accept = state() # accept state - creating a new instance of the class
+      initial = state() # initial state
+
+      initial.label = c # joins the initial to a character
+      initial.edgel = accept # edge1 is a pointer which points to the accept state
+
       newnfa = nfa(initial, accept)
       nfastack.append(newnfa)
 
+  # at this point, nfastack should have a single nfa on it
   return nfastack.pop()
 
+# Examples
 print(compile("ab.cd.|"))
 print(compile("aa.*"))
+print(compile("a?a.c*"))
+print(compile("(b.d)|(c+a*)."))
